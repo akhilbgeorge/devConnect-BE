2b45cd90fb898f90bfdb5a46f0c794ad654ef9d1
Akhil B George <akhilbgeorge97@gmail.com>
2025-10-12 03:48:27 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Node Js");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akhil", lastName: "B George" });
 });
-app.use("/",(req,res)=>{
-    res.send("Dashboard")
-})
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully!");
+});
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+app.use("/test", (req, res) => {
+  res.send("testing");
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
