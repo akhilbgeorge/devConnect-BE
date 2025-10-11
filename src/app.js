6816595bb441fdd90737961500a065279569142c
Akhil B George <akhilbgeorge97@gmail.com>
2025-10-12 04:37:25 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/abc", (req, res) => {
   res.send({ firstName: "Akhil", lastName: "B George" });
 });
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully!");
-});
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-app.use("/test", (req, res) => {
-  res.send("testing");
-});
+// app.get(/^\/ab?c$/, (req, res) => {
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+// app.get(/^\/ab+c$/, (req, res) => {
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+// app.get(/^\/ab*cd$/, (req, res) => {
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+// app.get(/^\/a(bc)?d$/, (req, res) => {
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+// app.get(/^\/a(bc)+d$/, (req, res) => {
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+
+// query params (/user?userId:123)
+// app.get("/user", (req, res) => {
+//   console.log(req.query)
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
+
+// dynamic routes (/user/123)
+// app.get("/user/:userId", (req, res) => {
+//   console.log(req.params)
+//   res.send({ firstName: "Akhil", lastName: "B George" });
+// });
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
