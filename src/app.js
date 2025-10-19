679be62617b911a584c0d6f49573d16a1d1501d5
Akhil B George <akhilbgeorge97@gmail.com>
2025-10-19 17:50:38 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -4,15 +4,37 @@ const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+app.get("/user", async(req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
